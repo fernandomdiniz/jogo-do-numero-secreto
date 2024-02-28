3a8280cb8fbd54568b8872ea593ee14c941164ba
fernando diniz <fernandodiniz@live.com>
2024-02-28 11:28:00 -0300

    parenteses e comentarios

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ function verificarChute() {
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        // adicionando parenteses 
+        let mensagemTentativas = (`Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`);
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
